cd97f2d0a03412151e3e47a544bfc58903cf6875
User <arjunrackad@gmail.com>
2022-04-29 19:45:25 +0530

    Update dashboard

--- src/constant.js
+++ src/constant.js
@@ -1,4 +1,4 @@
-// https://daily-tracker-project.herokuapp.com
+// http://localhost:5000
 export const Tracker_Url={
-    Server_url:'http://localhost:5000'
+    Server_url:'https://daily-tracker-project.herokuapp.com'
 }
