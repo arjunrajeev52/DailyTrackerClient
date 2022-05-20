40dddb8b235e7ead29a45d57aa36e59e4faf4559
User <arjunrackad@gmail.com>
2022-05-20 22:19:39 +0530

    Add new  type - Liability

--- src/App.js
+++ src/App.js
@@ -26,6 +26,7 @@ const TrackerData = {
     { key: 'f', text: 'SBI', value: 'SBI' },
     { key: 'o', text: 'FBI', value: 'FBI' },
     { key: 'c', text: 'Cash', value: 'Cash' },
+    { key: 'l', text: 'Liability', value: 'Liability' },
   ],
   toggleIncome: true,
   loaderFlag: false,
@@ -41,6 +42,13 @@ const TrackerData = {
     { key: 3, text: 'March', value: 3 },
     { key: 4, text: 'April', value: 4 },
     { key: 5, text: 'May', value: 5 },
+    { key: 5, text: 'June', value: 6 },
+    { key: 5, text:'July', value: 7 },
+    { key: 5, text: 'August', value: 8 },
+    { key: 5, text: 'Sebtember', value: 9 },
+    { key: 5, text: 'October', value: 10 },
+    { key: 5, text: 'November', value: 11 },
+    { key: 5, text: 'December', value: 12 },
   ],
   selectedMonth:new Date().getMonth()+1
 }
--- src/constant.js
+++ src/constant.js
@@ -1,4 +1,5 @@
 // http://localhost:5000
+// https://daily-tracker-project.herokuapp.com
 export const Tracker_Url={
-    Server_url:'https://daily-tracker-project.herokuapp.com'
+    Server_url:'http://localhost:5000'
 }
