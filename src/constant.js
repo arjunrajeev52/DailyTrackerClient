0464fb74034e1c4a70c9020794dd7707158a45d3
User <arjunrackad@gmail.com>
2022-05-23 12:11:59 +0530

    Add new  type - Liability

--- src/constant.js
+++ src/constant.js
@@ -1,5 +1,5 @@
 // http://localhost:5000
 // https://daily-tracker-project.herokuapp.com
 export const Tracker_Url={
-    Server_url:'http://localhost:5000'
+    Server_url:'https://daily-tracker-project.herokuapp.com'
 }
