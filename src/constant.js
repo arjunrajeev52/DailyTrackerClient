555b0dad3c0980c1fc1e370036f31cd63727fb1f
ARJUN RAJEEV <arjunrackad@gmail.com>
2024-01-01 23:48:09 +0530

    remove / from server url

--- src/constant.js
+++ src/constant.js
@@ -1,6 +1,5 @@
 // http://localhost:5000
 // https://daily-tracker-project.herokuapp.com
-// daily-tracker-server.netlify.app
 export const Tracker_Url={
-    Server_url:'https://daily-tracker-fdd4dcf9ce6e.herokuapp.com/'
+    Server_url:'https://daily-tracker-fdd4dcf9ce6e.herokuapp.com'
 }
